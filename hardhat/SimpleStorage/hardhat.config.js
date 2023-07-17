f2d6ae7b573ecb7d95c5f6fc8aa6f5ed299f120e
Hamza Rana <hamzarana75@gmail.com>
2023-07-18 04:14:25 +0500

    Updated 9h

--- hardhat/SimpleStorage/hardhat.config.js
+++ hardhat/SimpleStorage/hardhat.config.js
@@ -1,8 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config()
+require("@nomicfoundation/hardhat-verify");
+require("./tasks/block-number")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY=process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY
+
 module.exports = {
   defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost:{
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337.
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  },
   solidity: "0.8.7",
 };
--- hardhat/SimpleStorage/scripts/deploy.js
+++ hardhat/SimpleStorage/scripts/deploy.js
@@ -1,5 +1,5 @@
 const { ethers, run, network } = require("hardhat")
-
+require("dotenv").config();
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -7,11 +7,43 @@ async function main() {
     )
     console.log("Deploying contract...")
     const simpleStorage = await SimpleStorageFactory.deploy()
-    await simpleStorage.deployed()
+    await simpleStorage.waitForDeployment();
     console.log(simpleStorage)
-    console.log(deployedContract)
+    console.log("=====================================================")
+    console.log(simpleStorage.target)
+    console.log(network.config)
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.waitForDeployment()
+        await delay(60000);
+        await verify(simpleStorage.target, [])
+    }
+    const currentValue = await simpleStorage.get()
+    console.log(`Current Value is: ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.get()
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
+async function verify(contractAddress, args){
+    console.log("Verifying Contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")){
+            console.log("Already Verified")
+        } else{
+            console.log(e)
+        }
+    }
+}
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 main()
     .then(() => {
         process.exit(0)
